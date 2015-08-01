487b83eec6de9ce4b4679b1652bbe028d9dffaf2
turtle977 <turtle977@users.noreply.github.com>
2015-08-01 17:01:44 +0200

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -101,7 +101,7 @@ const unsigned THREAD_STACK_SIZE                             = 5 * 1024 * 1024;
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
+  77.108.0.245:50077",
   //"your_seed_ip2.com:8080",
 };
 
